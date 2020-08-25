8af4f200e7a2325b45f67124092969acf0b74c8f
ihor nesterov <igornes190@gmail.com>
2020-08-26 00:04:11 +0300

    Add project files.

---
+++ Poker/Card.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Poker
+{
+    enum CardSuit { pika = 1, chirva, buba, chresta };
+    enum CardValue { two = 2, three, four, five, six, seven, eight, nine, ten, J, Q, K, A };
+    class Card
+    {
+        private CardSuit suit;
+        private CardValue value;
+        private bool busy;
+        private string unique_id;
+        public static int count;
+
+        public CardSuit Suit { get; set; }
+
+        public CardValue Value { get; set; }
+
+        public bool Busy { get; set; }
+
+        public string Unique_id { get; set; }
+
+        public Card() { count++; }
+
+        public Card(CardSuit suit,CardValue value) : this()
+        {
+            Suit = suit;
+            Value = value;
+            int id = (int)Suit * 100 + (int)Value;
+            Unique_id = id.ToString();
+            Busy = true;
+        }
+
+        public string Print()
+        {
+            string output;
+            output = GetValue();
+            output += GetSuit();
+            output += "\r" + "Unique_id: " + Unique_id;
+            return output;
+        }
+
+        private string GetValue()
+        {
+            return Value switch
+            {
+                CardValue.two => "2",
+                CardValue.three => "3",
+                CardValue.four => "4",
+                CardValue.five => "5",
+                CardValue.six => "6",
+                CardValue.seven => "7",
+                CardValue.eight => "8",
+                CardValue.nine => "9",
+                CardValue.ten => "10",
+                CardValue.J => "J",
+                CardValue.Q => "Q",
+                CardValue.K => "K",
+                CardValue.A => "A",
+                _ => "",
+            };
+        }
+
+        private string GetSuit()
+        {
+            return Suit switch
+            {
+                CardSuit.pika => " Pika",
+                CardSuit.chirva => " Chirva",
+                CardSuit.chresta => " Kresta",
+                CardSuit.buba => " Buba",
+                _ => "",
+            };
+        }
+
+    }
+}
---
+++ Poker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card(CardSuit.pika, CardValue.three);
+            Card card1 = new Card(CardSuit.chirva, CardValue.five);
+            Card card2 = new Card(CardSuit.pika, CardValue.four);
+            Card card3 = new Card(CardSuit.chresta, CardValue.three);
+            Card card4 = new Card(CardSuit.buba, CardValue.Q);
+            Card card5= new Card(CardSuit.chirva, CardValue.A);
+            Card[] cards = { card, card1, card2, card3, card4, card5 };
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(cards[i].Print());
+                Console.WriteLine(Card.count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
